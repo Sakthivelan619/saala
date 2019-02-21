3d6c09cc22f4a558be8052763d63f4ac7ba0d718
kumar <kumarprasannacse@gmail.com>
2019-02-21 14:40:04 +0530

    dude's

---
+++ python.py
@@ -0,0 +1,10 @@
+              
+
+
+                             adavadi thooku durai 
+
+                              alaparai ana durai
+ 
+
+			yea adichi thooku adchi thooku
+			yea adichi thooku adchi thooku
